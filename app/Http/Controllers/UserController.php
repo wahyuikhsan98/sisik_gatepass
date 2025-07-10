33981816a39fec752dfbb08317a78762eaa940d0
Wahyu Ikhsan <136875175+wahyuikhsan98@users.noreply.github.com>
2025-07-10 22:56:17 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -241,7 +241,7 @@ class UserController extends Controller
                     $slugName = 'user'; // fallback jika slug kosong
                 }
     
-                $photoName = 'user' . '_' . time() . '.' . $extension;
+                $photoName = time() . '.' . $extension;
                 $destination = public_path('images/users');
     
                 // Pastikan folder tujuan ada
@@ -336,7 +336,7 @@ class UserController extends Controller
                     $slugName = 'user';
                 }
     
-                $photoName = 'user' . '_' . time() . '.' . $extension;
+                $photoName = time() . '.' . $extension;
                 $photoPath = 'images/users/' . $photoName;
     
                 // Buat folder jika belum ada
@@ -603,7 +603,7 @@ class UserController extends Controller
                 $slugName = 'user';
             }
     
-            $photoName = 'user' . '_' . time() . '.' . $extension;
+            $photoName = time() . '.' . $extension;
             $photoPath = 'images/users/' . $photoName;
     
             // Buat folder jika belum ada
