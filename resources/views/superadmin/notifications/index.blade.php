a55b4689085f6ac236759b0be4c1a953fda4f7ef
Wahyu Ikhsan <136875175+wahyuikhsan98@users.noreply.github.com>
2025-08-24 14:00:44 +0700

    Update index.blade.php

--- resources/views/superadmin/notifications/index.blade.php
+++ resources/views/superadmin/notifications/index.blade.php
@@ -18,8 +18,8 @@
                     <div class="page-inner py-5">
                         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                             <div>
-                                <h2 class="text-white pb-2 fw-bold">{{ $title }}</h2>
-                                <h5 class="text-white op-7 mb-2">Pengelolaan Data Notifikasi</h5>
+                                <h2 class="text-black pb-2 fw-bold">{{ $title }}</h2>
+                                <h5 class="text-black op-7 mb-2">Pengelolaan Data Notifikasi</h5>
                             </div>
                         </div>
                     </div>
