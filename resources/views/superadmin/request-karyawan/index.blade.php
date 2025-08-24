23b5d8aa365887a062cdfa6277e25b72b3cb4f4c
Wahyu Ikhsan <136875175+wahyuikhsan98@users.noreply.github.com>
2025-08-24 13:56:14 +0700

    Update index.blade.php

--- resources/views/superadmin/request-karyawan/index.blade.php
+++ resources/views/superadmin/request-karyawan/index.blade.php
@@ -24,7 +24,7 @@
 
         <div class="main-panel">
             <div class="content">
-                <div class="panel-header bg-primary-gradient">
+                <div class="panel-header panel-header-image">
                     <div class="page-inner py-5">
                         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                             <div>
@@ -536,6 +536,11 @@
     @include('layout.superadmin.script')
 
     <style>
+        .panel-header-image {
+            background: url('/assets/img/gambar gedung.jpg') no-repeat top center;
+            background-size: cover; /* supaya nutup penuh */
+        }
+        
         .table-container {
             position: relative;
             margin-top: 20px;
